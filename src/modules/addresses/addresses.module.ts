68890303bd4eab7cfa3863e195ecb67ef4f32b87
jzunigax2 <125698953+jzunigax2@users.noreply.github.com>
2026-08-06 08:50:05 -0600

    chore: remove Redis-related dependencies and configurations from the project

--- src/modules/addresses/addresses.module.ts
+++ src/modules/addresses/addresses.module.ts
@@ -1,23 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
 import { AccountModule } from '../account/account.module.js';
 import { AddressesController } from './addresses.controller.js';
-import { ThrottlerGuard } from '../../common/guards/throttler.guard.js';
 
 @Module({
-  imports: [
-    PassportModule,
-    AccountModule,
-    ThrottlerModule.forRoot({
-      // We are going to create a default throttler that can be overrided in any endpoint by doing
-      // @Throttle({ default: { limit: X, ttl: Y } })
-      throttlers: [{ name: 'default', limit: 60, ttl: 60_000 }],
-      storage: new ThrottlerStorageRedisService(process.env.REDIS_URL),
-    }),
-  ],
+  imports: [PassportModule, AccountModule],
   controllers: [AddressesController],
-  providers: [ThrottlerGuard],
 })
 export class AddressesModule {}
